4f412bda5adcd8c393ac5982d308f84235c319ba
KunoiSayami <46131041+KunoiSayami@users.noreply.github.com>
2020-09-20 13:41:12 +0800

    Optimize send message logic

--- watch.py
+++ watch.py
@@ -36,28 +36,41 @@ from pyrogram.types import Message
 @dataclass(init=False)
 class LastMessage:
     last_send: float
+    client: Client
+    group: List[str]
+    owner: int
     message: Message
 
-    def __init__(self, message: Message):
-        self.message = message
+    def __init__(self, owner: int, client: Client):
+        self.client = client
         self.last_send = time.time()
+        self.owner = owner
+        self.group = []
+        self.message = None
 
-    def update(self, message: Message) -> None:
-        self.message = message
-        self.last_send = time.time()
+    async def send_message(self, text: str) -> None:
+        if not self.check():
+            self.group = [text]
+            self.message = await self.client.send_message(self.owner, text, parse_mode='html')
+            self.last_send = time.time()
+        else:
+            self.group.append(text)
+            await self.message.edit('\n'.join(text), 'html')
 
     def check(self) -> bool:
-        return time.time() - self.last_send < 60
+        return time.time() - self.last_send > 60
 
 
 class Watcher:
-    def __init__(self, bot_token: str, api_id: int, api_hash: str, watch_list: List[int]):
+    def __init__(self, owner: int, bot_token: str, api_id: int, api_hash: str, watch_list: List[int]):
         self.logger: logging.Logger = logging.getLogger('Watcher')
         self.logger.setLevel(logging.DEBUG)
         self.human: Client = Client('watcher', api_id=api_id, api_hash=api_hash)
         self.bot: Client = Client('watchbot', api_id=api_id, api_hash=api_hash, bot_token=bot_token)
-        self.user_id: int = 0
+        self.owner: int = owner
         self.watch_list = watch_list
+        self.lock: asyncio.Lock = asyncio.Lock()
+        self.last_message: LastMessage = LastMessage(owner, self.bot)
 
     def init_handler(self) -> None:
         self.human.add_handler(MessageHandler(self.watch_media, filters.group
@@ -70,7 +83,7 @@ class Watcher:
         await self.bot.start()
         self.logger.debug('Starting main account')
         await self.human.start()
-        self.user_id = (await self.human.get_me()).id
+        # self.owner = (await self.human.get_me()).id
         self.logger.info('Start bot successfully')
 
     @staticmethod
@@ -82,11 +95,13 @@ class Watcher:
         await asyncio.gather(self.human.stop(), self.bot.stop())
 
     async def watch_text(self, _client: Client, msg: Message) -> None:
-        await self.bot.send_message(self.user_id, self.paste(msg), parse_mode='html')
+        async with self.lock:
+            await self.last_message.send_message(self.paste(msg))
         raise ContinuePropagation
 
     async def watch_media(self, _client: Client, msg: Message) -> None:
-        await self.bot.send_message(self.user_id, self.paste_media(msg), parse_mode='html')
+        async with self.lock:
+            await self.last_message.send_message(self.paste_media(msg))
         raise ContinuePropagation
 
     @staticmethod
@@ -101,21 +116,23 @@ class Watcher:
 
     @staticmethod
     def get_media_type(msg: Message) -> str:
-       return 'photo' if msg.photo else \
-           'video' if msg.video else \
-               'animation' if msg.animation else \
-                   'sticker' if msg.sticker else \
-                       'voice' if msg.voice else \
-                           'document' if msg.document else \
-                               'audio' if msg.audio else \
-                                   'contact' if msg.contact else 'error'
+        return 'photo' if msg.photo else \
+            'video' if msg.video else \
+            'animation' if msg.animation else \
+            'sticker' if msg.sticker else \
+            'voice' if msg.voice else \
+            'document' if msg.document else \
+            'audio' if msg.audio else \
+            'contact' if msg.contact else 'error'
 
 
 async def main():
     config = ConfigParser()
     config.read('config.ini')
-    watcher = Watcher(config.get('telegram', 'bot_token'), config.getint('telegram', 'api_id'),
-                      config.get('telegram', 'api_hash'), ast.literal_eval(config.get('follow', 'list')))
+    watcher = Watcher(
+        config.getint('telegram', 'owner'),
+        config.get('telegram', 'bot_token'), config.getint('telegram', 'api_id'),
+        config.get('telegram', 'api_hash'), ast.literal_eval(config.get('follow', 'list')))
     await watcher.start()
     await watcher.idle()
     await watcher.stop()
